0f5c844351c9bfe830e640d7ef7b67c7c40ce428
flor condori <condorihuamanif@gmail.com>
2017-04-20 10:50:54 -0500

    Desafio V

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-main
+var contendedor = document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var verde = document.getElementById("verde");
+var azul = document.getElementById("azul");
+var amarillo = document.getElementById("amarillo");
+
+contendedor.style.position = "relative";
+contendedor.style.background = "DarkGrey";
+contendedor.style.width = "600px";
+contendedor.style.padding = "50px";
+
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
